b5ecf3e25167ba952bb0a4783ee0af20d1c279e8
Hypericats <charlesolivier.malo@gmail.com>
2024-02-22 16:00:19 -0500

    added

---
+++ pull.bat
@@ -0,0 +1,4 @@
+git remote add origin https://github.com/Hypericat/iCanRepo.git
+git branch -M main
+git pull origin main
+set /p DUMMY=Hit ENTER to continue...
---
+++ push.bat
@@ -0,0 +1,7 @@
+set commit=%1
+git add .
+git commit -m "%commit%"
+git remote add origin https://github.com/Hypericat/iCanRepo.git
+git branch -M main
+git push -u origin main
+set /p DUMMY=Hit ENTER to continue...
